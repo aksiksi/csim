0473fc42aa4ef9e8961424e7d5c52cfbd8f342fb
Assil Ksiksi <cyph0nik@gmail.com>
2016-09-21 15:24:16 -0400

    Initial version of RandCircuit

--- src/test/scala/me/assil/cgen/RandCircuitSpec.scala
+++ src/test/scala/me/assil/cgen/RandCircuitSpec.scala
@@ -2,8 +2,9 @@ package me.assil.cgen
 
 import org.scalatest._
 
-class CircuitGenSpec extends FunSuite {
-  test("A CircuitGen must return") {
-    
+class RandCircuitSpec extends FunSuite {
+  test("A CircuitGen must return generate some stuff?") {
+    val gen = new RandCircuit(1000, 5, 2, "abc.out")
+    assert(1 == 1)
   }
 }
