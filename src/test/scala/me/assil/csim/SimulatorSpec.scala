e0554c42205c5447ef96e103cc6614b23ae5485c
Assil Ksiksi <cyph0nik@gmail.com>
2016-09-21 15:23:17 -0400

    Updated to match API changes in 5b526

--- src/test/scala/me/assil/csim/SimulatorSpec.scala
+++ src/test/scala/me/assil/csim/SimulatorSpec.scala
@@ -1,22 +1,22 @@
 package me.assil.csim
 
-import org.scalatest._
+import java.io.File
 
+import org.scalatest._
 import CircuitParser.parseInputFile
 
 object SimulatorSpec {
   def testCircuit(name: String): Boolean = {
-    val simFile = getClass.getResource(s"circuits/$name.ckt").getPath
-    val inputFile = getClass.getResource(s"circuits/$name.in").getPath
+    val simFile = new File(getClass.getResource(s"circuits/$name.ckt").getFile)
+    val inputFile = new File(getClass.getResource(s"circuits/$name.in").getFile)
+    val outFile = new File(getClass.getResource(s"circuits/$name.out").getFile)
 
-    val simulator = new Simulator(simFile)
+    val sim = new Simulator(simFile)
     val inputs = parseInputFile(inputFile)
 
-    val expected: Vector[Vector[Int]] = parseInputFile(
-      getClass.getResource(s"circuits/$name.out").getPath
-    ).map(_.map(_.value))
+    val expected: Vector[Vector[Int]] = parseInputFile(outFile).map(_.map(_.value))
 
-    val outputs = inputs.map(in => simulator.run(in).map(_.value))
+    val outputs = inputs.map(in => sim.run(in).map(_.value))
 
     outputs == expected
   }
@@ -31,13 +31,13 @@ class SimulatorSpec extends FunSuite {
   }
 
   test("A Simulator should evaluate the largest test circuit") {
-    val name = "s27"
+    val name = "s349f_2"
     assert(testCircuit(name))
   }
 
   test("A Simulator must throw an exception if a file is not found") {
-    val simFile = "not_real.ckt"
-    val inputFile = "not_real.in"
+    val simFile = new File("not_real.ckt")
+    val inputFile = new File("not_real.in")
 
     assertThrows[IllegalArgumentException] {
       new Simulator(simFile)
