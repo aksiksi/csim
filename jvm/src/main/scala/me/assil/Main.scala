dc89285b66c27f203a65e0f3df13bdf3157ac16e
Assil Ksiksi <cyph0nik@gmail.com>
2016-09-29 12:36:24 -0400

    Refined console app for Csim JVM

--- jvm/src/main/scala/me/assil/Main.scala
+++ jvm/src/main/scala/me/assil/Main.scala
@@ -43,30 +43,4 @@ object Main extends App {
     helpMessage()
   else
     processInputs()
-
-//  val inputs =
-//    Vector(
-//      Vector(Bit(0), Bit(0)),
-//      Vector(Bit(0), Bit(1)),
-//      Vector(Bit(1), Bit(0)),
-//      Vector(Bit(1), Bit(1))
-//    )
-//
-//  // Setup a test file
-//  val f = new File("test.txt")
-//  val fw = new FileWriter(f)
-//  fw.write("AND 1 2 3\n")
-//  fw.write("INPUT 1 2\n")
-//  fw.write("OUTPUT 3\n")
-//  fw.flush()
-//
-//  val lines = CircuitHelper.readFile(f)
-//
-//  val sim = new Simulator(lines)
-//
-//  val outputs = inputs.map(sim.run)
-//
-//  println(outputs)
-//
-//  f.delete()
 }
